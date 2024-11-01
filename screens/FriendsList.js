df92e1427c6a03affe9f301040470ad437359c09
AkimBudylyastov <73135649+Akimakimakim00@users.noreply.github.com>
2024-11-01 15:18:40 +0300

    Update FriendsList.js

--- screens/FriendsList.js
+++ screens/FriendsList.js
@@ -91,14 +91,6 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: '#b8b8b8',
         borderRadius: 15,
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.2,
-        shadowRadius: 1.41,
-        elevation: 2,
     },
     friendAvatar: {
         width: 50,
